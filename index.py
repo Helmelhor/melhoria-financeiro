d52cb1b46728c1feedfc450f10e297c3dd2ae8d6
Helmer Barcelos <snorlaxgamer17@gmail.com>
2024-12-24 20:03:03 -0300

    Update index.py
    adicionei dois botões, para duas respectivas funções (principais da aplicação)

--- index.py
+++ index.py
@@ -2,12 +2,6 @@ import streamlit as st
 
 st.title('Gerenciador Financeiro')
 
-img_botao1 = "https://e7.pngegg.com/pngimages/406/844/png-clipart-computer-icons-person-user-spark-icon-people-share-icon.png"
-
-botao_html = f"""
-<a href="https://www.example.com">
-        <img src="{img_botao1}" width="200" />
-    </a> 
-"""
-
-st.markdown(botao_html, unsafe_allow_html=True)
+st.button('Pessoa física')
+   
+st.button('Pessoa Juridica')
