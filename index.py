f4ca2ad24ee3721735b22acaff44b097aa2bfe88
Helmer Barcelos <snorlaxgamer17@gmail.com>
2024-12-24 18:20:12 -0300

    Update index.py
    testei a side bar, mas não é assim que funciona, então adicionei um título e estou colocando um botão com imagem

--- index.py
+++ index.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.sidebar('1','2')
+st.title('Gerenciador Financeiro')
+
+img_botao1 = "https://e7.pngegg.com/pngimages/406/844/png-clipart-computer-icons-person-user-spark-icon-people-share-icon.png"
+
+botao_html = f"""
+<a href="https://www.example.com">
+        <img src="{img_botao1}" width="200" />
+    </a> 
+"""
+
+st.markdown(botao_html, unsafe_allow_html=True)
