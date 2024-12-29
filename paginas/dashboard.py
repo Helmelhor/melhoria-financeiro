fe5c79fb4297d4b405c0096ee556ae298e56338f
Helmer Barcelos <snorlaxgamer17@gmail.com>
2024-12-29 20:29:50 -0300

    Update dashboard.py

--- paginas/dashboard.py
+++ paginas/dashboard.py
@@ -1,4 +1,4 @@
 import streamlit as st
-
-st.title("Dashboard")
-st.write("Aqui você pode visualizar o resumo financeiro!")
+def run():
+    st.title("Dashboard")
+    st.write("Aqui você pode visualizar o resumo financeiro!")
