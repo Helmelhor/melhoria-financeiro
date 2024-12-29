8308498bd7c39636481cc4eb4bb9897968050287
Helmer Barcelos <snorlaxgamer17@gmail.com>
2024-12-29 15:34:12 -0300

    Update index.py
    seguimento do fluxo proposto, descrito em papel

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-st.title('Gerenciador Financeiro')
+st.header('Bem-vindo ao Gerenciador Financeiro')
+st.subheader("Gerencie suas finanças de maneira simples e eficiente")
 
-st.button('Pessoa física')
-   
-st.button('Pessoa Juridica')
+botao_upload = st.button('Já tenho minha planilha')
+botao_inputs = st.button('Quero registrar minhas informações!')
+
+upload_csv = st.file_uploader('Escolha um Arquivo: ',type=["XLSX","CSV","PDF"])
