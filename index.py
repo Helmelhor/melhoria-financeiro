d673469a5fc81227522000ac6622f06abc92f223
helmer barcelos <snorlaxgamer17@gmail.com>
2025-02-10 21:06:26 -0300

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import sqlite3
-from senha import chave
 from google import genai
 from database import criar_tabela_metas, adicionar_meta, buscar_metas, atualizar_progresso
 
@@ -163,7 +162,7 @@ elif selected_page == "Metas financeiras":
 elif selected_page == "Análise de investimentos":
 
     # Configurando a API do Gemini
-    client = genai.Client(api_key = chave )
+    client = genai.Client(api_key = "AIzaSyBI2z3lHl9mdRLYZnKUum9Hrc5PL4kt-Q0" )
 
     # Inicializando a memória da sessão
     if "chat_history" not in st.session_state:
